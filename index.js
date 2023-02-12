31c4a6575debec9d4286b05ef7132d054cf24f4e
lulu_dex <palmellatimpsy@gmail.com>
2023-02-12 18:01:30 +0100

    javascript assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+function multiply(a, b){
+    return a * b;
+}
+
+console.log(multiply(2, 4));
+
+let firstName = "Emike";
+let lastName= "Ola-ayeni";
+let stuternTrack =  "Moblie app development";
+let skills= "reading";
+let favoriteColor="purple";
+let countriesIndepence="october 1";
+
+
+let description = `hello my name is ${firstName + " " + lastName},my student track is  ${stuternTrack}, my skills are ${skills},my favorite colour${favoriteColor},my countries independence${countriesIndepence}.
